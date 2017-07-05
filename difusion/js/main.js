825e274a63baa9ccbf1623c988709d7615088923
macramole <leandrogarber@gmail.com>
2017-07-04 21:20:37 -0300

    otra

--- difusion/js/main.js
+++ difusion/js/main.js
@@ -2,6 +2,8 @@ var CANT_IMAGENES = 59;
 var CANT_IMAGENES_GRANDES = 10;
 // var IMAGENES_GRANDES = [ 7, 18 ];
 
+var cantImagesLoaded = 0;
+
 $(function() {
     $('#fullpage').fullpage({
         scrollOverflow : true
@@ -14,10 +16,12 @@ $(function() {
         $("#imgs .wrapper").append($img);
 
         if ( i % ( Math.floor(CANT_IMAGENES/CANT_IMAGENES_GRANDES ) ) == 0 ) {
-            var $imgGrande = $("<img>").attr("src", "images/fotos/grandes/" + currentImagenGrande + ".JPG");
-            $imgGrande.addClass("grande");
-            $("#imgs .wrapper").append($imgGrande);
-            currentImagenGrande++;
+            if ( currentImagenGrande <= CANT_IMAGENES_GRANDES ) {
+                var $imgGrande = $("<img>").attr("src", "images/fotos/grandes/" + currentImagenGrande + ".JPG");
+                $imgGrande.addClass("grande");
+                $("#imgs .wrapper").append($imgGrande);
+                currentImagenGrande++;
+            }
         }
     }
     for ( var i = 1 ; i <= Math.floor(CANT_IMAGENES / 2) - 2 ; i++ ) {
@@ -31,10 +35,22 @@ $(function() {
       percentPosition: true,
       columnWidth : '.sizer'
     });
+    //
+    // $('#imgs .wrapper').imagesLoaded( function() {
+    // });
 
-    $(window).load(function() {
-          $('#imgs .wrapper').masonry('layout');
-    });
+    $('#imgs .wrapper').imagesLoaded()
+         .always( function( instance ) {
+             $('#imgs .wrapper').masonry('layout');
+         })
+        .progress( function( instance, image ) {
+            cantImagesLoaded++;
+
+            if ( cantImagesLoaded % 15 == 0 ) {
+                $('#imgs .wrapper').masonry('layout');
+            }
+        });
+    ;
 
     $("#imgs .wrapper").css("margin-top", $("header").outerHeight());
 });
